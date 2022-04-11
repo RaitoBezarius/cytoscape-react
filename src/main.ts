b8dc180ad2dfafa2be548b457f50910b18208b48
Ryan LAHFA <ryan.lahfa@justice.gouv.fr>
2022-04-11 16:10:18 +0200

    chore: clean up old components, maximize modularity, functionify all components with hooks

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,9 @@
 import { GraphWrapper } from './graph_wrapper';
 import { NodeWrapper } from './node_wrapper';
 import { EdgeWrapper } from './edge_wrapper';
-import { Node } from './node';
-
-export function Graph() { return null; }
-export function Edge() { return null; }
-
 
 export default {
 	GraphWrapper,
 	NodeWrapper,
-	EdgeWrapper,
-	Graph,
-	Node,
-	Edge
+	EdgeWrapper
 };
