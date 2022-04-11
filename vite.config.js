3b870cfa78e57eb4eec4dac88f0fef16dd16c895
Ryan LAHFA <ryan.lahfa@justice.gouv.fr>
2022-04-11 16:10:18 +0200

    chore: move to Vite, produce UMD/ES bundles, enable TypeScript

---
+++ vite.config.js
@@ -0,0 +1,28 @@
+// vite.config.js
+import path from 'path'
+const { defineConfig } = require('vite')
+
+module.exports = defineConfig({
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/main.ts'),
+      name: 'cytoscape-react',
+      fileName: (format) => `cytoscape-react.${format}.js`
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['react', 'react-dom', 'cytoscape', 'cytoscape-dom-node'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          reactDom: 'ReactDOM',
+          react: 'React',
+          cytoscape: 'Cytoscape',
+          "cytoscape-dom-node": 'CyDomNode'
+        }
+      }
+    }
+  }
+})
